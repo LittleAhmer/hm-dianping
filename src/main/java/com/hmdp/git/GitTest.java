44bb754d4bf0e95b433f9671c952c9c6abc2b8d3
Antone <Antone@github.com>
2023-02-05 10:19:37 +0800

    my 3 idea commite！

--- src/main/java/com/hmdp/git/GitTest.java
+++ src/main/java/com/hmdp/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello github!");
         System.out.println("Hello github! I am change!");
+        System.out.println("Hello github! I am change!");
     }
 }
